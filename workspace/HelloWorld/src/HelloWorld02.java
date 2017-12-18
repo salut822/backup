2bd1bbbf3b266368489d735e6802db22ead0577c
salut822 <vaa.srts12@ezweb.ne.jp>
2017-12-18 20:18:43 +0900

    コミットします。

---
+++ workspace/HelloWorld/src/HelloWorld02.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author testuser
+ *
+ */
+public class HelloWorld02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String s=new String();
+		s="Hello";
+		System.out.println(s);
+
+	}
+
+}
---
+++ workspace/TestProject01/src/HelloWorld.java
@@ -0,0 +1,27 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		String s="123";
+		System.out.println(s);
+		int i=Integer.parseInt(s);
+		System.out.println(i);
+
+		String t="456";
+		System.out.println(t);
+		int n=Integer.parseInt(t);
+		System.out.println(n);
+
+		String r="789";
+		System.out.println(r);
+		int o=Integer.parseInt(r);
+		System.out.println(o);
+
+		String g="101112";
+		System.out.println(g);
+		int l=Integer.parseInt(g);
+		System.out.println(l);
+
+	}
+}
