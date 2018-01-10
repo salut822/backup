b9bc020c0828b5a60d7575f21806757feb6a2e3d
salut822 <vaa.srts12@ezweb.ne.jp>
2018-01-10 22:09:15 +0900

    01/10

---
+++ DBConnector/src/DBConnector.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+*<p>MySQlに接続するためのユーティクラスです。<br>
+*ルートアカウントにてDBに接続されます。</p>
+*/
+public class DBConnector {
+	/**
+	 * JDBC ドライバー名
+	 */
+private static String driverName="com.mysql.jdbc.Driver";
+/**
+ * データベース接続URL
+ */
+private static String url=
+"jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
+
+/**
+ * データベース接続ユーザー名
+ */
+private static String user="root";
+
+/**
+ * データベース接続パスワード
+ */
+private static String passwaord="mysql";
+
+public Connection getConnection(){
+	Connection con=null;
+	try{
+		Class.forName(driverName);
+		con=(Connection)DriverManager.getConnection(url,user,passwaord);
+	}catch(ClassNotFoundException e){
+		e.printStackTrace();
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+	return con ;
+
+	}
+}
+
+
+
