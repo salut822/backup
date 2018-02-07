35de979fd690fdc6ffc4d28ce260356f59c8053b
salut822 <vaa.srts12@ezweb.ne.jp>
2018-02-07 21:48:08 +0900

    !

--- TestDB/src/ChkLoadJdbc.java
+++ TestDB/src/ChkLoadJdbc.java
@@ -1,16 +1,28 @@
+/**
+ *
+ */
 
+/**
+ * @author testuser
+ *
+ */
 public class ChkLoadJdbc {
-	public static void main(String[] args) throws InstantiationException,IllegalAccessException{
-	  String msg="";
-	  try{
-		  Class.forName("com.mysql.jdbc.Driver").newInstance();
-		  msg="ドライバのロードに成功しました";
-	  }catch (ClassNotFoundException e){
-		  msg="ドライバのロードに失敗しました";
-	  }
-      System.out.println(msg);
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		String msg = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			msg = "成功";
+		} catch (ClassNotFoundException e) {
+			msg = "失敗";
+		}
+		System.out.println(msg);
+
+		/**
+		 * @param args
+		 */
+
+		// TODO 自動生成されたメソッド・スタブ
+
 	}
+
 }
-
-
-
